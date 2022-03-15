62690590d7607ae27c4bfd0aa95e90137088d700
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-03-15 20:09:01 -0300

    AttFinal

--- js.js
+++ js.js
@@ -1,31 +1,45 @@
-let input = document.getElementsByTagName('input')[0]
-let participantes  = document.getElementById('divParticipantes')
-let btn = document.getElementById('btn')
-let add = document.getElementsByTagName('button')[0]
-let pessoas = []
-let result = document.getElementById('divGanhador')
-let div = document.getElementById('divSorteio')
-valor = -1
-numero = 0
+let input = document.getElementsByTagName("input")[0];
+let participantes = document.getElementById("divParticipantes");
+let btn = document.getElementById("btn");
+let add = document.getElementsByTagName("button")[0];
+let pessoas = [];
+let result = document.getElementById("divGanhador");
+let div = document.getElementById("divSorteio");
+let ganhador = document.getElementById("ganhador");
+valor = -1;
+numero = 0;
 
-add.addEventListener('click',()=>{
-    if(input.value === ""){
-        document.getElementById('erro').innerHTML = "<p>Insira um nome</p>"
-    }else{
-    document.getElementById('erro').innerHTML = ""
-    pessoas.push(`${input.value}`)
-    valor++
-    numero++
-    participantes.innerHTML += `<p>${numero} - ${pessoas[valor]}</p>`
-    input.value = ""
-}
-})
-btn.addEventListener('click',()=>{
-    result.style.display = "flex"
-    result.style.animationName = "result"
-    div.style.display = "none"
-
-    result.innerHTML
-})
-
-
+add.addEventListener("click", () => {
+  if (input.value === "") {
+    document.getElementById("erro").innerHTML = "<p>Insira um nome</p>";
+  } else {
+    document.getElementById("erro").innerHTML = "";
+    pessoas.push(`${input.value}`);
+    valor++;
+    numero++;
+    participantes.innerHTML += `<p>${numero} - ${pessoas[valor]}</p>`;
+    input.value = "";
+    document.getElementById(
+      "length"
+    ).innerHTML = `<p>Quantidade de participantes: ${pessoas.length}`;
+  }
+});
+btn.addEventListener("click", () => {
+  if (pessoas.length <= 1) {
+    document.getElementById(
+      "length"
+    ).innerHTML = `<p>Insira pelo menos 2 participantes</p>`;
+  } else {
+    result.style.animationName = "result";
+    div.style.animationName = "calculo";
+    setInterval(() => {
+      result.style.display = "flex";
+      div.style.display = "none";
+    }, 1200);
+    let numero = parseInt(Math.random() * pessoas.length)
+    ganhador.innerHTML = `<H2>Parabéns ao <br>ganhador:</H2><h2>${pessoas[numero]}</h2>`;
+    console.log(pessoas[numero]);
+    console.log(numero)
+    console.log(pessoas.length)
+  }
+});
